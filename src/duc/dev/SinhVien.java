9cb21a328084e122ff4fa8c31e799a8cadeb9ed3
duc <trinhminhduc.0500@gmail.com>
2018-10-10 00:35:09 +0700

    sinhvien_gioi

--- src/duc/dev/SinhVien.java
+++ src/duc/dev/SinhVien.java
@@ -1,23 +1,22 @@
 package duc.dev;
-public class SinhVien {
-	  
-	private String name;
-	private double Toan;
-	private double Ly;
-	private double Hoa;
-		
-	public SinhVien(String name, double Toan, double Ly, double Hoa) {	
-		
-	this.name = name;
-	this.Toan = Toan;
-	this.Ly = Ly;
-	this.Hoa = Hoa;
-	
-			}
-    public double DiemTB(double Toan, double Ly, double Hoa) {
-   	 return (Toan + Ly + Hoa) / 3;
-		}
-    public void DiemTB() {
-	  System.out.println(name+":"+DiemTB(Toan, Ly, Hoa));
-	}
-} 
+ public class JavaOOP {
+	public static void main(String[] args) {
+ 				
+  		final String arrName[] = {"Toan", "luan", "Anh"};
+  		
+ 		final double arrToan[] = { 6, 8, 8};
+ 		final double arrLy[] = { 7, 9, 8};
+ 		final double arrHoa[] = {7, 10,8};
+ 		
+ 		SinhVien arrSinhVien[] = new SinhVien[arrName.length];
+ 		 for(int i = 0; i < arrSinhVien.length; i++) {
+ 			SinhVien sv = new SinhVien(arrName[i], arrToan[i], arrLy[i], arrHoa[i]);
+ 				arrSinhVien[i]=sv;
+ 		}
+ 		 for(int i = 0; i < arrSinhVien.length; i++) {
+ 			if(arrSinhVien[i].DiemTB(arrToan[i], arrLy[i], arrHoa[i]) > 8) {
+ 				arrSinhVien[i].Show();
+ 			}
+ 		}
+ 	}
+  }
